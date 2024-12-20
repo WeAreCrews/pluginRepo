2a4f44555a31245ff0c6da090d8b5385b4678904
WeAreCrews <119337925+WeAreCrews@users.noreply.github.com>
2024-12-20 19:55:18 +0300

    Final Fix for NeoFetch (VenFetch)

--- types.ts
+++ types.ts
@@ -729,13 +729,13 @@ export const presetPlugins: PluginInfo[] = [
         customInfo: "Custom information specific to Example Plugin 1."
     },
     {
-        name: "NeoFetch",
-        filename: "neofetch",
-        filesearch: "neofetch",
-        downloadUrl: "https://git.nin0.dev/userplugins/venfetch/raw/branch/main/index.ts",
+        name: "NeoFetch (VenFetch)",
+        filename: "venfetch",
+        filesearch: "venfetch",
+        downloadUrl: "https://github.com/WeAreCrews/venfetch.git",
         downloadFiles: ["index.ts"],
         description: "Jealous of your Arch-using friends that seem to have no hobby beyond showing their neofetch? This can easily be solved thanks to venfetch, a plugin allowing you to show your superiority over your vanilla discord using friends. Simply enable the plugin and run /venfetch in a chat. It will send a neofetch-style message containing every useful detail about your Vencord install!",
-        tags: ["cool, arch, neofetch"],
+        tags: ["neofetch"],
         dateAdded: "1734209767",
         options: {},
         customInfo: "Custom information specific to Example Plugin 1."
